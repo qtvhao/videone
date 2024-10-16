a3aa927353b5f24ef646c15be886067f181dfae5
willwilson <qtvhao@gmail.com>
2024-10-16 10:16:31 +0700

    Refactor CloudflaredService to start and stop tunnel with updated URL and URL path parameters

--- studio/src/cloudflared/cloudflared.service.ts
+++ studio/src/cloudflared/cloudflared.service.ts
@@ -1,35 +1,57 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
-import { tunnel } from 'cloudflared';
+import * as childProcess from 'child_process';
 
 @Injectable()
 export class CloudflaredService implements OnModuleDestroy {
   private childProcess;
 
   async startTunnel() {
-    console.log('Cloudflared Tunnel Example.');
-
     try {
-      const { url, connections, child, stop } = tunnel({ '--hello-world': null });
+      // Check if cloudflared is installed
+      const check = childProcess.spawnSync('cloudflared', ['--version']);
+      if (check.error) {
+        throw new Error('Cloudflared is not installed.');
+      }
+      console.log('Starting Cloudflared tunnel...');
+      const child = childProcess.spawn(
+        'cloudflared',
+        ['tunnel', '--url', 'http://localhost:3000'],
+        {
+          // stdio: 'inherit',
+        },
+      );
+      // console.log('Cloudflared tunnel started.');
 
       // Save child process to stop it later
       this.childProcess = child;
 
       // Show the URL
+      const url = new Promise((resolve) => {
+        const url_regex = /\|\s+(https?:\/\/[^\s]+)/;
+        let parser = (data: Buffer) => {
+          const match = url_regex.exec(data.toString());
+          if (match) {
+            resolve(match[1]);
+          }
+        };
+        child.stdout.on('data', parser);
+        child.stderr.on('data', parser);
+      });
       console.log('LINK:', await url);
 
-      // Wait for all 4 connections to be established
-      const conns = await Promise.all(connections);
+      // // Wait for all 4 connections to be established
+      // const conns = await Promise.all(connections);
 
-      // Show the connections
-      console.log('Connections Ready!', conns);
+      // // Show the connections
+      console.log('Connections Ready!');
 
-      // Stop the tunnel after 15 seconds (optional)
-      setTimeout(stop, 15_000);
+      // // Stop the tunnel after 15 seconds (optional)
+      // //   setTimeout(stop, 15_000);
 
-      // Handle exit
-      child.on('exit', (code) => {
-        console.log('Tunnel process exited with code', code);
-      });
+      // // Handle exit
+      // child.on('exit', (code) => {
+      //   console.log('Tunnel process exited with code', code);
+      // });
     } catch (error) {
       console.error('Failed to start Cloudflared tunnel:', error);
     }
