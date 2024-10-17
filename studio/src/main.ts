67909e6bb381d4cc6e5347353d230e39a5a7b56a
willwilson <qtvhao@gmail.com>
2024-10-17 18:21:20 +0700

    Refactor video table to update video status in editItem method and handle invalid messages in checkBackendAPIAvailable method

--- studio/src/main.ts
+++ studio/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('App is running on http://localhost:3000');
+  });
 }
 bootstrap();
